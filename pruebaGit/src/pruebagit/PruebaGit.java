4a08d6d6270338336d1113a9b38de8150a6ea8b7
Admin <Admin@Lenovo-Adri>
2022-06-12 14:37:07 +0200

    tus muertos x2

---
+++ pruebaGit/src/pruebagit/PruebaGit.java
@@ -0,0 +1,11 @@
+package pruebagit;
+
+public class PruebaGit {
+
+    public static void main(String[] args) {
+        
+        System.out.println("hola");
+        System.out.println("adios");
+    }
+    
+}
