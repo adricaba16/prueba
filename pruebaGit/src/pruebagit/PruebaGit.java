8268ddbc765691df3097cb6e14e976684a0e5098
Admin <Admin@Lenovo-Adri>
2022-06-12 14:44:17 +0200

    ahora un sout

--- pruebaGit/src/pruebagit/PruebaGit.java
+++ pruebaGit/src/pruebagit/PruebaGit.java
@@ -3,13 +3,14 @@ package pruebagit;
 public class PruebaGit {
 
     public static void main(String[] args) {
-        
+
         System.out.println("hola");
         System.out.println("adios");
         int hola = 10;
         if (hola > 5) {
             hola = 20;
+            System.out.println(hola);
         }
     }
-    
+
 }
